64449baeb9a1894e2ecf802a3f32bafeef8b9219
jdsm3011-alt <jdsm3011@gmail.com>
2026-07-24 18:57:23 +0100

    Add files via upload

--- js/modules/workspaces.js
+++ js/modules/workspaces.js
@@ -1,7 +1,1 @@
-/* === MÓDULO: WORKSPACES === */
-/* Workspace state creation, cloning, persistence, apply,
-   tabs UI, switch, close, create new, initialize */
-/* Origem: 05-app-main.js linhas 105-445 */
-(function(){
-  // Código a migrar de 05-app-main.js
-})();
+(function(){}());
